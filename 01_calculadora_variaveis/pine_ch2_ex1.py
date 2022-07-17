de0782451c140ab6f8d307ff89b78c13391209bf
adaaalbert <adalbert.chemistry@gmail.com>
2022-07-17 11:22:40 -0300

    Introduction to Python Science & Eng (David Pine) - chp2_ex1

---
+++ 01_calculadora_variaveis/pine_ch2_ex1.py
@@ -0,0 +1,18 @@
+# Introduction to Python Science & Eng. - Pine
+# Exercises - chapter 2
+# A ball is thrown vertically up in the air from a height h0 above
+# the ground at an initial velocity v0 . Its subsequent height h and
+# velocity v are given by the equations
+# h = h0 + v0t - 1/2gt²
+# v = v0 - gt
+# where g = 9.8 m/s², the accelaration due to gravity
+# This script calculates the height h and velocity v at a time t
+# after the ball is thrown.
+h0 = 1.6        # meter
+v0 = 14.2       # m/s, meters/second
+t = 2.0         # time in s
+g = 9.8         # m/s²
+
+h = h0 + v0*t - (g*t**2)/2
+v = v0 - g*t
+
