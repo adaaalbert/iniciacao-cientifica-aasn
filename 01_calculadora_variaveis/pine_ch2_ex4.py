811a6c008b1719ea377dd6c337366b5f5e6f4d08
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2022-07-18 20:38:50 -0300

    Primeira correção exercício 4, capítulo 2
    - Usando a função `complex` para converter o input
    - Colocando parênteses nos denominadores

--- 01_calculadora_variaveis/pine_ch2_ex4.py
+++ 01_calculadora_variaveis/pine_ch2_ex4.py
@@ -2,21 +2,16 @@
 # ax² + bx + c = 0
 # has two solutions given by the quadratic formula
 # x = ((-b +- (b² - 4ac)^1/2)/2a)
-import numpy as np
+from cmath import sqrt
 
 # Input the constant values
-a = float(input("Digit the 'a' value."))
-b = float(input("Digit the 'b' value."))
-c = float(input("Digit the 'c' value."))
-
-# Fixing to work with complex numbers
-a = a + 0j
-b = b + 0j
-c = c + 0j
+a = complex(input("Digit the 'a' value."))
+b = complex(input("Digit the 'b' value."))
+c = complex(input("Digit the 'c' value."))
 
 # Calculate the 2 solutions
-x1 = (-b + (np.sqrt(b**2 - 4*a*c))) / 2*a
-x2 = (-b - (np.sqrt(b**2 - 4*a*c))) / 2*a
+x1 = (-b + sqrt(b**2 - 4*a*c)) / (2*a)
+x2 = (-b - sqrt(b**2 - 4*a*c)) / (2*a)
 
 print(x1)
 print(x2)
