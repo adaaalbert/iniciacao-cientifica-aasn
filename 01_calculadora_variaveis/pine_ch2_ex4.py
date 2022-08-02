8f008cdfd62130142a2b7fd0bfedd4631872ae09
Adalberto Alves <adalbert.chemistry@gmail.com>
2022-08-02 20:20:53 -0300

    English translation & 1st refatoration

--- 01_calculadora_variaveis/pine_ch2_ex4.py
+++ 01_calculadora_variaveis/pine_ch2_ex4.py
@@ -1,21 +1,22 @@
-# A quadratic equation with the general form:
-# ax² + bx + c = 0
-# has two solutions given by the quadratic formula
-# x = ((-b +- (b² - 4ac)^1/2)/2a)
+"""
+A quadratic equation with the general form:
+ax² + bx + c = 0
+has two solutions given by the quadratic formula
+x = ((-b +- (b² - 4ac)^1/2)/2a)
+"""
 from cmath import sqrt
 
 
-def roots(a, b, c):
+def roots(coef_x2, coef_x, const):
     """Calculate the 2 solutions"""
-    x1 = (-b + sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-    x2 = (-b - sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-    return x1, x2
+    root1 = (-coef_x + sqrt(coef_x ** 2 - 4 * coef_x2 * const)) / (2 * coef_x2)
+    root2 = (-coef_x - sqrt(coef_x ** 2 - 4 * coef_x2 * const)) / (2 * coef_x2)
+    return root1, root2
 
 
 if __name__ == '__main__':
-    # Input the constant values
+    # Input coefficients and constant values
     a = complex(input("Digit the 'a' value."))
     b = complex(input("Digit the 'b' value."))
     c = complex(input("Digit the 'c' value."))
-
     print(roots(a, b, c))
