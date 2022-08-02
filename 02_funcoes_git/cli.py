e3b2b4338f414a0abea9d28219db2ceef36ed08b
Adalberto Alves <adalbert.chemistry@gmail.com>
2022-08-02 20:24:12 -0300

    English translation & documentation

--- 02_funcoes_git/cli.py
+++ 02_funcoes_git/cli.py
@@ -1,9 +1,21 @@
-from lancamento_vertical import height
+"""Simple script for vertical throw calculation
 
-altura_inicial = float(input("Forneça a altura inicial / m: "))
-velocidade_inicial = float(input("Forneça a velocidade inicial / m/s: "))
-tempo = float(input("Forneça o tempo / s: "))
+Example: A ball is thrown vertically up in the air from an initial height above
+the ground at an initial speed. Its subsequent height and speed are given by the
+following equations:
+height = initial_height + initial_speed * time - (1/2) * GRAVITY * time**2
+speed = initial_speed - GRAVITY * time
+where GRAVITY = 9.8 m/s², the acceleration due to gravity
 
-altura = height(altura_inicial, velocidade_inicial, tempo)
+This script calculates the height and speed at a time after the ball is thrown.
+"""
 
-print(f"Altura de {altura:.2f} m")
+from vertical_throw import height
+
+initial_height = float(input("Enter the initial height / m: "))
+initial_speed = float(input("Enter the initial speed / m/s: "))
+time = float(input("Enter the time / s: "))
+
+new_height = height(initial_height, initial_speed, time)
+
+print(f"Height: {new_height:.2f} m")
