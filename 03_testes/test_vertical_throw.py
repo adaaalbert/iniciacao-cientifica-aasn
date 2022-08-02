2c1beee52874cad6c1ac3d9132131f5b1f5d4858
Adalberto Alves <adalbert.chemistry@gmail.com>
2022-08-02 20:27:17 -0300

    English translation

--- 03_testes/test_vertical_throw.py
+++ 03_testes/test_vertical_throw.py
@@ -1,26 +1,26 @@
-from lancamento_vertical import altura
+from vertical_throw import height
 from pytest import approx
 
 
-def test_tempo_0_velocidade_inicial_0():
-    assert altura(5, 0, 0) == 5
+def test_time_0_initial_speed_0():
+    assert height(5, 0, 0) == 5
 
 
-def test_tempo_0_velocidade_inicial_1():
-    assert altura(5, 1, 0) == 5
+def test_time_0_initial_speed_1():
+    assert height(5, 1, 0) == 5
 
 
-def test_velocidade_inicial_0_altura_inicial_0_tempo_1():
-    assert altura(0, 0, 1) == -4.9
+def test_initial_speed_0_initial_height_0_time_1():
+    assert height(0, 0, 1) == -4.9
 
 
-def test_velocidade_inicial_0_altura_inicial_0_tempo_2():
-    assert altura(0, 0, 2) == -19.6
+def test_initial_speed_0_initial_height_0_time_2():
+    assert height(0, 0, 2) == -19.6
 
 
-def test_velocidade_inicial_0_altura_inicial_1_tempo_2():
-    assert altura(1, 0, 2) == -18.6
+def test_initial_speed_0_initial_height_1_time_2():
+    assert height(1, 0, 2) == -18.6
 
 
-def test_velocidade_inicial_1_altura_inicial_0_tempo_1():
-    assert altura(0, 1, 1) == approx(-3.9, rel=0, abs=0.001)
+def test_initial_speed_1_initial_height_0_time_1():
+    assert height(0, 1, 1) == approx(-3.9, rel=0, abs=0.001)
