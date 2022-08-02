882e33b829198738453808250b0a8eb10fbfcb3c
Adalberto Alves <adalbert.chemistry@gmail.com>
2022-08-02 20:25:04 -0300

    English translation & 1st version of documentation

--- 02_funcoes_git/vertical_throw.py
+++ 02_funcoes_git/vertical_throw.py
@@ -1,5 +1,11 @@
+"""
+This script defines a function height and returns a calculated height given the
+time and initial speed of an object based on vertical throw equation.
+"""
 GRAVITY = 9.8  # m/s**2
 
 
 def height(initial_height, initial_speed, time):
+    """Returns the height of a vertical throw after given time and speed.-"""
+
     return initial_height + initial_speed * time - (1/2) * GRAVITY * time**2
