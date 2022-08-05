d885cbf6e1ae784f174d632066a6ce7eb51de795
Adalberto Alves <adalbert.chemistry@gmail.com>
2022-08-05 17:26:55 -0300

    Initial commit

---
+++ 04_ambientes_virtuais_unidades/vertical_throw_pint.py
@@ -0,0 +1,23 @@
+"""
+This script defines a function height and returns a calculated height given the
+time and initial speed of an object based on vertical throw equation.
+"""
+import pint
+
+ureg = pint.UnitRegistry()
+Q_ = ureg.Quantity
+
+GRAVITY = Q_(9.8, 'm/s**2')
+
+
+@ureg.wraps('m', ('m', 'm/s', 's'))
+def height(initial_height, initial_speed, time):
+    """Returns the height of a vertical throw after given time and speed.-"""
+
+    return initial_height + initial_speed * time - (1/2) * GRAVITY.magnitude * time**2
+
+
+if __name__ == '__main__':
+    print(height('5 m', '1 m/s', '0 s'))
+    print(height('500 cm', '1 m/s', '0 s'))
+    print(height('50 ft', '10 in/s', '1000 ms'))
