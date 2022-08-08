6fd65af45b6d3aff0d51c21df8adc19ec377c810
Adalberto Alves <adalbert.chemistry@gmail.com>
2022-08-08 17:14:20 -0300

    mistype correction

--- 03_testes/vertical_throw.py
+++ 03_testes/vertical_throw.py
@@ -1,7 +1,6 @@
-git log = 9.8  # m/s^2
+GRAVITY = 9.8  # m/s^2
 
 
 def height(initial_height, initial_speed, time):
-    return (initial_height +
-            initial_speed * time -
+    return (initial_height + initial_speed * time -
             1 / 2 * GRAVITY * time ** 2)
