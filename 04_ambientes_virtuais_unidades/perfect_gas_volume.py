1e84b6df8750f81128efb9f6f3dcd5dca4b50ae2
Adalberto Alves <adalbert.chemistry@gmail.com>
2022-08-13 11:13:00 -0300

    Test with different constant values

--- 04_ambientes_virtuais_unidades/perfect_gas_volume.py
+++ 04_ambientes_virtuais_unidades/perfect_gas_volume.py
@@ -1,13 +1,15 @@
 import pint
+from scipy.constants import gas_constant
 
 ureg = pint.UnitRegistry()
 Q_ = ureg.Quantity
 
 # GAS_CONSTANT = Q_(8.314462618, 'J/(K * mol)')
-GAS_CONSTANT = Q_(0.082057366080960, 'atm * l / (mol * K)')
+#GAS_CONSTANT = Q_(0.082057366080960, 'atm * l / (mol * K)')
+gas_constant = gas_constant * ureg('(m**3 * Pa)/(K*mol)')
 
-
-@ureg.wraps('liter/mol', ('K', 'atm'))
+# @ureg.wraps('liter/mol', ('K', 'atm'))
+@ureg.wraps('liter/mol', ('K', 'Pa'))
 def perfect_gas_volume(temperature, pressure):
     """
     Parameters
@@ -20,8 +22,9 @@ def perfect_gas_volume(temperature, pressure):
 
     """
 
-    return GAS_CONSTANT.magnitude * temperature / pressure
+    return gas_constant.magnitude * temperature / pressure
 
 
 if __name__ == '__main__':
-    print(perfect_gas_volume('273 K', '1 atm'))
+    # print(perfect_gas_volume('273 K', '1 atm'))
+    print(perfect_gas_volume('273 K', '101325 Pa'))
